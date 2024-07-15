42d79738b0048a6e728b98672c61fb6e2ef6316f
FURI <63885192+furidngrt@users.noreply.github.com>
2024-07-15 13:27:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// App.js
+
 import React from 'react';
-import './App.css'; // Impor CSS khusus Anda di sini
-import 'bootstrap/dist/css/bootstrap.min.css'; // Impor Bootstrap CSS di sini
+import './App.css'; // Import Bootstrap CSS di sini
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS di sini
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -9,9 +11,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <Router>
-      <div className="App d-flex flex-column min-vh-100">
+      <div className="App">
         <Header />
-        <div className="content flex-grow-1">
+        <div className="content">
           <Routes>
             <Route exact path="/" element={<Home />} />
           </Routes>

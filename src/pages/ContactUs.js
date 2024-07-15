8904418f62ec546d2627cb739e78ae332acd16fb
FURI <63885192+furidngrt@users.noreply.github.com>
2024-07-15 13:32:43 +0700

    Create ContactUs.js

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,15 @@
+// ContactUs.js
+
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Contact Us</h2>
+      <p>If you have any questions, feel free to reach out to us!</p>
+      <p>Email: contact@cryptotracker.com</p>
+    </div>
+  );
+};
+
+export default ContactUs;
